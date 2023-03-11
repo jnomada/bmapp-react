cadecf93dd1db2b5d3bdbc9b8fa142708cb7e17e
jnomada <46501701+jnomada@users.noreply.github.com>
2023-03-11 20:54:51 +0100

    delete expenses (#3)

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -3,6 +3,13 @@ import classes from './Expense.module.css';
 const Expense = (props) => {
   return (
     <tr className={classes.expense}>
+      <td>
+        <input 
+          value={props.expenseId} 
+          type="checkbox" 
+          onChange={props.onSelect}
+        />
+      </td>
       <td>{props.date}</td>
       <td>{props.type}</td>
       <td>{props.description}</td>
--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -2,13 +2,15 @@ import classes from './ExpenseFilter.module.css';
 
 const ExpenseFilter = (props) => {
 
+  const today = new Date().toISOString().substring(0,7);
+
   const onDateChange = (e) => {
     props.onDateChange(new Date(e.target.value));
   }
 
   return (
     <div className={classes['expense-filter']}>
-      <input type="month" id="date" name="date" onChange={onDateChange}/>
+      <input type="month" id="date" name="date" defaultValue={today} onChange={onDateChange}/>
     </div>
   )
 }
